da3628fa7ae3225ec87239a0020cad2f61d47c2d
Marcelo Janke <marcelojanke@icloud.com>
2017-10-20 19:57:49 -0200

    Vamo Registra

--- app/src/main/java/com/example/marcelojanke/myapplication/MainActivity.java
+++ app/src/main/java/com/example/marcelojanke/myapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.marcelojanke.myapplication;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
@@ -13,9 +16,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
 
         BtnMapa = (Button) findViewById(R.id.btnMapa);
+
+
         BtnMapa.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,5 +36,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    public void SendMessage(View view) {
+        Intent i = new Intent(this,Contato.class);
+        startActivity(i);
     }
 }
